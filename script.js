ac5c1bfff98def4cb3039129e0f2a54b79fbc6ec
Luís Felipe <luisf.csdev@gmail.com>
2024-06-16 15:28:35 -0300

    feat: build blu-links (#2)

--- script.js
+++ script.js
@@ -1,12 +1,68 @@
-function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle("light")
+const THEME_KEY = '@devlinks:theme-state-1.0.0'
 
-  const img = document.querySelector("#profile img")
+const PREFERS_THEME_DARK = '(prefers-color-scheme: dark)'
 
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
-  } else {
-    img.setAttribute("src", "./assets/avatar.png")
-  }
+function getCurrentTheme() {
+  const rootClassList = document.documentElement.classList
+  return rootClassList.contains('light') ? 'light' : 'dark'
 }
+
+function toggleTheme() {
+  const rootClassList = document.documentElement.classList
+  rootClassList.toggle('light')
+}
+
+function handleThemeToggle() {
+  toggleTheme()
+
+  const currentTheme = getCurrentTheme()
+  localStorage.setItem(THEME_KEY, currentTheme)
+}
+
+function getSystemTheme() {
+  if (!window.matchMedia) {
+    return
+  }
+
+  const isDark = window.matchMedia(PREFERS_THEME_DARK).matches
+  return isDark ? 'dark' : 'light'
+}
+
+function setInitialTheme() {
+  const storedTheme = localStorage.getItem(THEME_KEY)
+  const theme = storedTheme || getSystemTheme()
+
+  if (theme !== 'light') {
+    return
+  }
+
+  toggleTheme()
+}
+
+function setCurrentThemeToSystemTheme(isDarkEvent) {
+  const storedTheme = localStorage.getItem(THEME_KEY)
+  if (storedTheme) {
+    return
+  }
+
+  const systemTheme = isDarkEvent.matches ? 'dark' : 'light'
+  const currentTheme = getCurrentTheme()
+
+  if (currentTheme === systemTheme) {
+    return
+  }
+
+  toggleTheme()
+}
+
+function setAutoThemeToggle() {
+  const prefersDark = window.matchMedia(PREFERS_THEME_DARK)
+  prefersDark.addEventListener('change', setCurrentThemeToSystemTheme)
+}
+
+function setThemeSettings() {
+  setInitialTheme()
+  setAutoThemeToggle()
+}
+
+setThemeSettings()
